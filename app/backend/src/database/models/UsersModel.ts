8d48192620a57be186c2532398420dc53bca0e0a
Gilmara <gilmaralopes2006@gmail.com>
2023-04-01 02:56:11 -0300

    [Feat]:rocket: Requisito 6

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,41 @@
+import { DataTypes, InferAttributes, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model<InferAttributes<Users>> {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default Users;
