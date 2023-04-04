a4e3c943cfee72c57c9505e9903cc939dd4667ee
Gilmara <gilmaralopes2006@gmail.com>
2023-04-04 14:44:41 -0300

    [Feat]:rocket: Iniciando os teste da rota /matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,32 @@
+import * as sinon from 'sinon';
+import { Model } from "sequelize";
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import MatchesModel from '../database/models/MatchesModel';
+import Matches from './mocks/matchesMock';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Rota /matches', () => {
+  afterEach(sinon.restore);
+
+  describe('GET matches', () => {
+    it('Deve retornar a lista de partidas', async () => {
+      sinon.stub(MatchesModel, 'findAll').resolves(Matches as unknown as MatchesModel[]);
+
+      const response = await chai.request(app).get('/matches');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(Matches);
+    });
+
+  })
+
+});
